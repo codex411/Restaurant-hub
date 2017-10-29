e24a406a9dc90c545e08ccaaa61bd4043d9d37ae
Codex411 <centerforward411@gmail.com>
2017-10-29 12:50:19 +0500

    Data Binding

---
+++ Assignment 2/conFusion/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dishdetail/dishdetail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,    
+    MatToolbarModule, 
+    MatListModule,   
+    MatGridListModule, 
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
