c1b1f754451b1a44cb7cec3cd9d0f9471721b2b7
Codex411 <centerforward411@gmail.com>
2017-10-29 16:05:39 +0500

    Angular Router Basics

--- Assignment 2/conFusion/src/app/app.module.ts
+++ Assignment 2/conFusion/src/app/app.module.ts
@@ -7,9 +7,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +34,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,   
     MatGridListModule, 
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
