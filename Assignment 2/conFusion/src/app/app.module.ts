fda7c12ac08f7b8e428f6b129ba9befc6d3cfdb2
Codex411 <centerforward411@gmail.com>
2017-10-29 18:15:48 +0500

    Single Page Applications Part 1

--- Assignment 2/conFusion/src/app/app.module.ts
+++ Assignment 2/conFusion/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
@@ -37,7 +38,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
