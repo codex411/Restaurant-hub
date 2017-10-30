088ab888c184c3ab9f9a7d3bbc2e485e639bd485
Codex411 <centerforward411@gmail.com>
2017-10-30 18:57:40 +0500

    Reactive Forms Part 1

--- Assignment 3/conFusion/src/app/app.module.ts
+++ Assignment 3/conFusion/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule, MatCardModule, MatDialogModule, MatCheckboxModule, MatFormFieldModule, MatInputModule } from '@angular/material'; 
+import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule, MatCardModule,
+  MatDialogModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
+  MatSelectModule, MatSlideToggleModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -42,9 +45,12 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatDialogModule,
     MatCheckboxModule,
+    MatSelectModule, 
+    MatSlideToggleModule,
     FlexLayoutModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule
   ],
