216ef93f6239069338a5323bcf15fd8d6d19d9a4
Codex411 <centerforward411@gmail.com>
2017-10-31 16:15:19 +0500

    Http Part 1

---
+++ Assignment 4/conFusion/src/app/app.module.ts
@@ -0,0 +1,77 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule, MatCardModule,
+  MatDialogModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
+  MatSelectModule, MatSlideToggleModule, MatProgressSpinnerModule, MatSliderModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+import { baseURL } from './shared/baseurl';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,    
+    MatToolbarModule, 
+    MatListModule,   
+    MatGridListModule, 
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatSelectModule, 
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService,
+    ProcessHTTPMsgService,
+    {provide: 'BaseURL', useValue: baseURL}
+  ],
+  entryComponents: [
+    LoginComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Assignment 4/conFusion/src/app/services/dish.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Dish } from '../shared/dish';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DishService {
+
+  constructor(private http: Http,
+    private processHTTPMsgService: ProcessHTTPMsgService) { }
+
+  getDishes(): Observable<Dish[]> {
+    return this.http.get(baseURL + 'dishes')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); });
+  }
+
+  getDish(id: number): Observable<Dish> {
+    return  this.http.get(baseURL + 'dishes/'+ id)
+                    .map(res => { return this.processHTTPMsgService.extractData(res); });
+  }
+
+  getFeaturedDish(): Observable<Dish> {
+    return this.http.get(baseURL + 'dishes?featured=true')
+                    .map(res => { return this.processHTTPMsgService.extractData(res)[0]; });
+  }
+
+  getDishIds(): Observable<number[]> {
+    return this.getDishes()
+      .map(dishes => { return dishes.map(dish => dish.id) });
+  }
+}
