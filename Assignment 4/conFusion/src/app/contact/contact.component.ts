9e4f038080bb1ca1eec1df197882d9b8a6c4213e
Codex411 <centerforward411@gmail.com>
2017-11-01 23:15:04 +0500

    Assignment 4

--- Assignment 4/conFusion/src/app/contact/contact.component.ts
+++ Assignment 4/conFusion/src/app/contact/contact.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Feedback, ContactType } from '../shared/feedback';
-import { flyInOut } from '../animations/app.animation';
+import { flyInOut, expand } from '../animations/app.animation';
+
+import { FeedbackService } from '../services/feedback.service';
 
 @Component({
   selector: 'app-contact',
@@ -13,14 +15,17 @@ import { flyInOut } from '../animations/app.animation';
     'style': 'display: block;'
   },
   animations: [
-    flyInOut()
+    flyInOut(),
+    expand()
   ]
 })
 export class ContactComponent implements OnInit {
 
   feedbackForm: FormGroup;
   feedback: Feedback;
+  feedbackCopy: Feedback = null;  
   contactType = ContactType;
+  spinnerVisibility: boolean = false;
 
   formErrors = {
     'firstname': '',
@@ -50,7 +55,8 @@ export class ContactComponent implements OnInit {
     },
   };
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private feedbackService: FeedbackService) {
     this.createForm();
   }
 
@@ -91,8 +97,18 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit() {
-    this.feedback = this.feedbackForm.value;
-    console.log(this.feedback);
+    this.spinnerVisibility = true;
+    this.feedbackCopy = this.feedbackForm.value;
+    this.feedbackService.submitFeedback(this.feedbackCopy)
+      .subscribe(feedback => 
+        { setTimeout(() => 
+          {
+            this.feedback = feedback; this.spinnerVisibility = false; console.log(this.feedback); 
+            setTimeout(() => this.feedback = null, 5000);
+          }
+          , 2000);
+        }
+      );
     this.feedbackForm.reset({
       firstname: '',
       lastname: '',
